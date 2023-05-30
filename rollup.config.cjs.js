e552ae39cd397e28158fd368dd1f9f63cc79c85f
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-05-30 16:24:12 +0300

    build: configure rollup

---
+++ rollup.config.cjs.js
@@ -0,0 +1,8 @@
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "lib/cjs/index.js",
+    format: "cjs",
+    sourcemap: true,
+  },
+};
---
+++ rollup.config.esm.js
@@ -0,0 +1,8 @@
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "lib/esm/index.js",
+    format: "esm",
+    sourcemap: true,
+  },
+};
